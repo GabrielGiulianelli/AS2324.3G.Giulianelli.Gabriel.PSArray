7244af6401097c07e6aacdf8657fa8085c0da046
PC15 <PC15@INFO5-2558>
2024-02-29 08:37:49 +0100

    fit/ Funzione che crea una tabella con i voti e i pesi

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Giulianelli Gavbriel 3G verifica informatica");
+            StampaVotiPesi(voti, pesi, nvoti);
             
+        }
+
+
+        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            Console.WriteLine("Voti  Pesi");
+            for(int i = 0; i < pesi.Length; i++)
+            {
+                Console.WriteLine(voti[i]+ pesi[i]);    
+            }
         }
     }
 }
