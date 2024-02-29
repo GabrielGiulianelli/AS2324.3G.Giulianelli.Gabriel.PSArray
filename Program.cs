f2b54514e5a86a289c279f37c15179b8b65e2cbf
PC15 <PC15@INFO5-2558>
2024-02-29 08:53:28 +0100

    fit/  seconda funzione in cui vengono caricati casualmente i voti e i pesi nella tabella della prima funzione

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Giulianelli Gavbriel 3G verifica informatica");
+            Console.WriteLine("Inserire il numero di voti");
+            int nvoti=Convert.ToInt32(Console.ReadLine());
+            double[] voti = new double[nvoti];
+            int[]pesi= new int[nvoti];
+            CaricaVettori(ref voti, ref pesi, nvoti);
             StampaVotiPesi(voti, pesi, nvoti);
             
         }
@@ -13,9 +18,19 @@
         static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
             Console.WriteLine("Voti  Pesi");
-            for(int i = 0; i < pesi.Length; i++)
+            for(int i = 0; i < nVoti; i++)
             {
-                Console.WriteLine(voti[i]+ pesi[i]);    
+                Console.WriteLine($"{voti[i]}       {pesi[i]}");    
+            }
+        }
+        static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+        {
+            Random random = new Random();
+           
+            for(int i=0; i <nVoti;i++)
+            {
+                voti[i] = random.Next(0, 11);
+                pesi[i] = random.Next(0, 101);
             }
         }
     }
